84dc06c8464d095eaa7f8c51f6f1b0e2ca4a39d8
adriann1410 <adriann11@interia.eu>
2018-01-23 23:35:29 +0100

    message class to transfer data

---
+++ src/main/Message.java
@@ -0,0 +1,50 @@
+package main;
+
+public class Message {
+	
+
+	int idReceiver;
+	int idSender;
+	String subject;
+	String content;
+	
+	public Message(int ir, int is, String sub, String con){
+		this.idReceiver = ir;
+		this.idSender = is;
+		this.subject = sub;
+		this.content = con;
+		
+	}
+	
+	public int getIdReceiver() {
+		return idReceiver;
+	}
+
+	public void setIdReceiver(int idReceiver) {
+		this.idReceiver = idReceiver;
+	}
+
+	public int getIdSender() {
+		return idSender;
+	}
+
+	public void setIdSender(int idSender) {
+		this.idSender = idSender;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
